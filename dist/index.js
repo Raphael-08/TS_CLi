e5f1d06119c82ff1cf2f5003e94ba09482bd70ac
Raphael-08 <106021733+Raphael-08@users.noreply.github.com>
2024-03-15 21:45:41 +0530

    just some tweaking!

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { init } from "./commands/init";
+import { Command } from "commander";
+async function main() {
+    const program = new Command()
+        .name("ts_cli")
+        .description("add components and dependencies to your project")
+        .version("1.0.0", "-v, --version", "display the version number");
+    program.addCommand(init);
+    program.parse();
+}
+main();
---
+++ src/commands/init.ts
@@ -0,0 +1,57 @@
+import ora from "ora"
+import { Command } from "commander"
+import { execa } from "execa"
+import { detect } from "@antfu/ni"
+
+
+const PROJECT_DEPENDENCIES = [
+    "cowsay"
+  ]
+
+export async function getPackageManager(
+    targetDir: string
+  ): Promise<"yarn" | "pnpm" | "bun" | "npm"> {
+    const packageManager = await detect({ programmatic: true, cwd: targetDir })
+  
+    if (packageManager === "yarn@berry") return "yarn"
+    if (packageManager === "pnpm@6") return "pnpm"
+    if (packageManager === "bun") return "bun"
+  
+    return packageManager ?? "npm"
+  }
+
+export const init = new Command()
+    .name("init")
+    .description("install dependencies")
+    .option(
+        "-c, --cwd <cwd>",
+        "the working directory. defaults to the current directory.",
+        process.cwd()
+    )
+    .action(async ({ cwd }: { cwd: string }) => {
+        try {
+            await runInit(cwd);
+        } catch (error) {
+            console.error("Initialization failed:", error);
+            process.exit(1);
+        }
+    });
+
+export async function runInit(cwd: string) {
+    const dependenciesSpinner = ora(`Installing dependencies...`)?.start()
+    const packageManager = await getPackageManager(cwd)
+
+        // TODO: add support for other icon libraries.
+    const deps = [
+        ...PROJECT_DEPENDENCIES
+    ]
+
+    await execa(
+        packageManager,
+        [packageManager === "npm" ? "install" : "add", ...deps],
+        {
+        cwd,
+        }
+    )
+    dependenciesSpinner?.succeed()
+}
